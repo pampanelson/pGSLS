bb261d12ad9f8278f9c513ed882ceeab8328350d
pampa nelson <pampa.nelson@gmail.com>
2018-08-06 22:13:19 +0800

    remove redundant part for only make color

--- src/ofApp.h
+++ src/ofApp.h
@@ -57,15 +57,10 @@ public:
 	ftFbo				cameraFbo;
 	ofParameter<bool>	doFlipCamera;
 	
-	
-	// opencv
-	ofxCv::ContourFinder 	contourFinder;
-	ofColor 				targetColor;
-	ofImage					grayImg;
+
 	
 	// for debug contour =======================
 	
-	int					contourIndex = -1;
 	
 	// Time
 	float				lastTime;
@@ -88,40 +83,18 @@ public:
 	ofFbo               flowFbo;
 	ofFbo               obsticleFbo;
 	
-	// sound
-	
-	void 				audioIn(ofSoundBuffer &inBuffer);
-	//	void				audioIn(float *input, int bufferSize, int nChannels);
-	ofSoundStream 		soundStream;
-	ofSoundBuffer 		audioInSoundBuffer;
-	ofSoundBuffer		chennalBuffer;
-	ofxFft* 			fft;
-	float * 			fftData;
-	
-	float 				rms;
-	float 				power;
-	
-	vector <float> 		left;
-	vector <float> 		right;
-	
-	
+
 	
 	// test
 	
-	ofImage				imgTest1;
-	ofImage				imgTest3;
+
 	ofImage				obsticleImg; // invert of back image
 	ofImage				backImg; // show rest writtings
 	bool				drawTestImage = false;
 	bool				drawBackImg = true;
 	
 	
-	vector<vector<cv::Point>>  contourBlobs;
-	vector<ofImage>		vecContourBlobImagesForFlow;
-	vector<ofVec4f>		vecContourBlobImagesBoudingBox;
-	int					drawImageForFlowMilSecs = 1000;
-	int					deltaSpeedFactor = 20;// how fast a contour blob move for make flow,smaller is quicker
-	vector<int>			vecDrawImageForFlowOnce;
+
 	
 	
 	
@@ -134,8 +107,6 @@ public:
 	bool				bDrawGui = true;
 	
 	
-	void				goNextFlow();
-	void				showAudioDebug();
 	
 	void keyPressed(int key);
 	void keyReleased(int key);
